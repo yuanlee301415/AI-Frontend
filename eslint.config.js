5f9f09e830cb28aa4394fbccc857575fb490b7bd
yuanlee <yuanlee301415@126.com>
2025-05-19 20:55:05 +0800

    chore: Forked from `FVue3-TS-Tailwind-Template@v1.7`

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+
+  // 添加全局规则配置
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+]
---
+++ scripts/__viewTemplate.tpl
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+
+</script>
+
+<template>
+  <div>
+    <h2>${capitalNames} - ${title}</h2>
+  </div>
+</template>
+
+<style scoped lang="less"></style>
